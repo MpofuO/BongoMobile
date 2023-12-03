b4b784df1c8694aed74510e56d2bc06c59164b22
OMpofu <mpofuoliver1109@gmail.com>
2023-12-03 23:48:47 +0200

    First Commit

---
+++ Bongo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Bongo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Bongo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Bongo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
